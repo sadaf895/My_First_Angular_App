0a408b85b07282894e5764021b27bb3472c95059
sadaf895 <116058905+sadaf895@users.noreply.github.com>
2023-06-26 19:40:05 +0530

    MyFirstAngularCommit

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Superman' },
+  { id: 13, name: 'IronMan' },
+  { id: 14, name: 'Dr.Megan' },
+  { id: 15, name: 'Batman' },
+  { id: 16, name: 'RubberMan' },
+  { id: 17, name: 'Dynama' },
+  { id: 18, name: 'Dr. IQ' },
+  { id: 19, name: 'Mr.Evans' },
+  { id: 20, name: 'Tornado' }
+];
